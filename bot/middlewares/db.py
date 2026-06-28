fc287e2e67808a2155be65840c5bdfbf7b3769be
KRYZENSYS <f91186645@gmail.com>
2026-06-28 08:07:08 +0500

    feat: DB session middleware

---
+++ bot/middlewares/db.py
@@ -0,0 +1,18 @@
+"""Inject DB session into handler kwargs."""
+from typing import Any, Awaitable, Callable, Dict
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from bot.database.engine import get_session
+
+
+class DbSessionMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        async with get_session() as session:
+            data["session"] = session
+            return await handler(event, data)
